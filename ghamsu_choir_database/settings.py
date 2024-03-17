d2a51dded0a93ccc960b9d44a686c6eb2f455366
Okyesco <okyesco0549677744@gmail.com>
2024-03-17 00:23:52 +0000

    First commit

--- ghamsu_choir_database/settings.py
+++ ghamsu_choir_database/settings.py
@@ -32,7 +32,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w=yz^^^n5y_dnm*w+zh_^=s$u&!8j6x+)++ookgex&42w@+7mm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
